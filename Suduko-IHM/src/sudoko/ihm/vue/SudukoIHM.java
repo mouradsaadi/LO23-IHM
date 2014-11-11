0d61f350c6af9673d2e2fd1e5457ae7fc55a68e5
mouradsaadi <blackstonedrummer@gmail.com>
2014-11-11 16:53:07 +0100

    Adding mainContainer of IHM

--- Suduko-IHM/src/sudoko/ihm/vue/SudukoIHM.java
+++ Suduko-IHM/src/sudoko/ihm/vue/SudukoIHM.java
@@ -23,7 +23,6 @@ public class SudukoIHM extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-        
         stage.setScene(scene);
         stage.show();
     }
