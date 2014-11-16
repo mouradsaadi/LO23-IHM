903913a8eaa2513092991e9000dee783eebfc0d7
marfel <felixmarin@live.fr>
2014-11-16 20:20:33 +0100

    no message

--- Suduko-IHM/src/sudoko/ihm/vue/FXMLDocumentController.java
+++ Suduko-IHM/src/sudoko/ihm/vue/FXMLDocumentController.java
@@ -9,12 +9,13 @@ package sudoko.ihm.vue;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import org.controlsfx.dialog.Dialogs;
 import sudoku.ihm.model.data.sample.DataSample;
 
 /**
@@ -26,7 +27,12 @@ public class FXMLDocumentController implements Initializable {
     
     //Data
     public DataSample instance;
-        
+    public Dialogs dialog;
+    @FXML
+    private Button fillEmpty;
+    
+    @FXML
+    private Button fromGrid;
     @FXML   private Label userName;
     @FXML   private ImageView avatar;
     @FXML   private Button connexion;
@@ -38,5 +44,19 @@ public class FXMLDocumentController implements Initializable {
         userName.setText(instance.a.getPseudo());
         //avatar.setImage(new Image(instance.a.getProfilePicturePath()));
         connexion.setText("Connected");
+                fillEmpty.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+            Dialogs.create().title("Message").masthead("Message d'information")
+                .message("Message xdxxxx")
+                .showInformation();
+            }
+        });
+        fromGrid.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                dialog.title("Message");
+                dialog.message("Ca marche aussi :D");
+                dialog.showInformation();
+            }
+        });
     }    
 }
