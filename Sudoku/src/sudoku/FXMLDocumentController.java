3d73c69097bc3e83e5e944d8804497d71523360f
mouradsaadi <blackstonedrummer@gmail.com>
2014-11-20 15:10:42 +0100

    mainframe

---
+++ Sudoku/src/sudoku/FXMLDocumentController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sudoku;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author MOURAD
+ */
+public class FXMLDocumentController implements Initializable {
+    
+
+    
+}
